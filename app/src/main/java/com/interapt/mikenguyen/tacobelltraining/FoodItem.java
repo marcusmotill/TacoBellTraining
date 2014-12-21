95a3318838b669dcbdd3243b20cdaad000f7b0d8
Mike Nguyen <mikenguyen@Mikes-MacBook-Pro.local>
2014-12-21 02:12:04 -0500

    Splash page, main menu

---
+++ app/src/main/java/com/interapt/mikenguyen/tacobelltraining/FoodItem.java
@@ -0,0 +1,12 @@
+package com.interapt.mikenguyen.tacobelltraining;
+
+/**
+ * Created by mikenguyen on 12/21/14.
+ */
+public class FoodItem {
+    private int idNumber;
+    private int numberOfSteps;
+    private String[] stepsImages;
+    private String[] stepsStringContent;
+
+}
---
+++ app/src/main/java/com/interapt/mikenguyen/tacobelltraining/MainMenuActivity.java
@@ -0,0 +1,61 @@
+package com.interapt.mikenguyen.tacobelltraining;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.List;
+
+
+public class MainMenuActivity extends Activity {
+    private static final int SPEECH_REQUEST = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        startActivityForResult(intent, SPEECH_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == SPEECH_REQUEST && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+            // Do something with spokenText.
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
